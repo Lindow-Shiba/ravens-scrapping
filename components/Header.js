a2203e7932bbc59dd6a66cb14e03ce3402eb907a
Lindow-Shiba <rapidtuber12@gmail.com>
2025-07-12 22:21:20 +0100

    Add files via upload

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,1 @@
-import Link from 'next/link';
-import Image from 'next/image';
-
-export default function Header(){
-  return(
-    <header>
-      <Image src="/logo.png" alt="logo" width={34} height={34}/>
-      <h1>Raven's Scrap & Supply</h1>
-      <nav>
-        <Link href="/">Materials</Link>
-        <Link href="/database">Database</Link>
-        <Link href="/material-price">Admin Prices</Link>
-      </nav>
-    </header>
-  )
-}
+export default()=>null
